19ac7a9d6d2cb174cd23024bf7981d80074b5a63
Surjith <surjithctly@gmail.com>
2020-09-21 17:31:14 +0530

    postcss-clean

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,8 @@ module.exports = {
   plugins: [
     require(`tailwindcss`)(`./tailwind.config.js`),
     require(`autoprefixer`),
+    ...(process.env.NODE_ENV === "production"
+      ? [require(`postcss-clean`)]
+      : []),
   ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,7 @@ module.exports = {
   },
   theme: {
     extend: {
-      colors: {
-        primary: {
-          100: "#176AEF",
-        },
-      },
+      colors: {},
     },
   },
   variants: {},
