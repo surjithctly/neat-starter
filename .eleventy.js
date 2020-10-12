e3338ccd846af253769eb4c536acbb16c9778c40
Surjith <surjithctly@gmail.com>
2020-10-12 20:50:18 +0530

    fix netlify backend

--- .eleventy.js
+++ .eleventy.js
@@ -31,14 +31,14 @@ module.exports = function (eleventyConfig) {
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
     "./_tmp/static/css/style.css": "./static/css/style.css",
-    "./admin/config.yml": "./admin/config.yml",
+    "./src/admin/config.yml": "./admin/config.yml",
     "./node_modules/alpinejs/dist/alpine.js": "./static/js/alpine.js",
     "./node_modules/prismjs/themes/prism-tomorrow.css":
       "./static/css/prism-tomorrow.css",
   });
 
   // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("./static/img");
+  eleventyConfig.addPassthroughCopy("./src/static/img");
 
   // Let Eleventy transform HTML files as nunjucks
   // So that we can use .html instead of .njk
