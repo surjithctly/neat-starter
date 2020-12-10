c95d578b1a22d4067da1a712f13ecbc72974c382
Mark <mark@>
2020-12-10 19:16:47 +0000

    feat: add favicon handling to config along with a placeholder favicon

--- .eleventy.js
+++ .eleventy.js
@@ -40,6 +40,9 @@ module.exports = function (eleventyConfig) {
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
 
+  // Copy favicon to route of /_site
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+
   // Let Eleventy transform HTML files as nunjucks
   // So that we can use .html instead of .njk
   return {
